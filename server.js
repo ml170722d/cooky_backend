634ee43b9b2116ec2326078513c2ba3e16e34a69
Luka Matovic <ml170722d@student.etf.bg.ac.rs>
2021-04-09 01:32:39 +0200

    new endpoints

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+const e = require('express');
 const express = require('express');
-const { register, unregister, signin } = require('./modules/dbOper');
+const dbmod = require('./modules/dbmod');
 
 const app = express();
 
@@ -7,38 +8,75 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const port = 8081;
+const appName = '/cooky';
 
-app.post('/register', (req, res) => {
-    register(req.body)
-        .then(success => res.sendStatus(success))
-        .catch(fail => res.sendStatus(fail));
+app.post(`${appName}/register`, (req, res) => {
+    dbmod.register(req.body).then(_ => {
+        res.sendStatus(200);
+    }).catch(err => {
+        console.error(err);
+        res.sendStatus(401);
+    });
 });
 
-app.delete('/unregister', (req, res) => {
-    unregister(req.body)
-        .then(success => res.sendStatus(success))
-        .catch(err => {
-            if (err instanceof Error) {
-                console.error(err);
-                res.sendStatus(500);
-            } else {
-                res.sendStatus(err);
-            }
-        });
+app.delete(`${appName}/unregister`, (req, res) => {
+    dbmod.unregister(req.body).then(_ => {
+        res.sendStatus(200);
+    }).catch(err => {
+        console.error(err);
+        res.sendStatus(401);
+    });
 });
 
-app.post('/signin', (req, res) => {
-    signin(req.body)
-        .then(success => res.sendStatus(success))
-        .catch(err => {
-            if (err instanceof Error) {
-                console.error(err);
-                res.sendStatus(500);
-            } else {
-                res.sendStatus(err);
-            }
+app.post(`${appName}/signin`, (req, res) => {
+    dbmod.signin(req.body).then(_ => {
+        res.sendStatus(200);
+    }).catch(err => {
+        console.error(err);
+        res.sendStatus(401);
+    });
+});
+
+app.post(`${appName}/save`, (req, res) => {
+    dbmod.saveRecipe(req.body).then(_ => {
+        res.sendStatus(200);
+    }).catch(err => {
+        console.error(err);
+        res.sendStatus(401);
+    });
+});
+
+app.post(`${appName}/edit`, (req, res) => {
+    dbmod.editRecipe(req.body.q, req.body.data).then(_ => {
+        res.sendStatus(200);
+    }).catch(err=>{
+        console.error(err);
+        res.sendStatus(401);
+    });
+});
+
+app.delete(`${appName}/delete`, (req, res) => {
+    dbmod.deleteRecipe(req.body).then(_ => {
+        res.sendStatus(200);
+    }).catch(err => {
+        console.error(err);
+        res.sendStatus(401);
+    });
+});
+
+app.get(`${appName}/search`, (req, res) => {
+    dbmod.search('recipe_' + req.body.lang, req.body.query).then(cursor => {
+        return cursor.toArray();
+    }).then(list => {
+        list.forEach(e => {
+            delete e._id;
         });
-})
+        res.json(list);
+    }).catch(err => {
+        console.error(err);
+        res.sendStatus(401);
+    });
+});
 
 const serverListener = app.listen(port, () => {
     console.log(`Server running on port ${port}`);
